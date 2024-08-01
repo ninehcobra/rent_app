5ca31257599d1e4984d763da55cf72fb1790333b
ninehcobra <ttbexinhtt2903@gmail.com>
2024-08-01 09:58:02 +0700

    core/setup: setup source mobile

---
+++ lib/common_libs.dart
@@ -0,0 +1,4 @@
+/// Consolidate imports that are common across the app.
+library;
+
+export 'package:flutter/material.dart';
---
+++ lib/styles/colors.dart
@@ -0,0 +1,50 @@
+import 'package:rent_app/logic/common/color_utils.dart';
+import '../common_libs.dart';
+
+class AppColors {
+  /// Common
+  final Color primaryColor = const Color(0xFF000A8ED9);
+  final Color onPrimaryColor = Colors.white;
+  final Color secondaryColor = const Color(0xFF0084CCF5);
+  final Color onSecondaryColor = Colors.white;
+  final Color backgroundColor = const Color(0xFF00F9F9F9);
+  final Color blackColor = const Color(0xFF000000);
+  final Color greyMediumColor = const Color(0xFF00858585);
+  final Color whiteColor = Colors.white;
+  final Color errorColor = Color(0xFF00E13F32);
+  final Color onErrorColor = Colors.white;
+
+  final bool isDark = false;
+
+  Color shift(Color c, double d) =>
+      ColorUtils.shiftHsl(c, d * (isDark ? -1 : 1));
+
+  ThemeData getThemeData() {
+    /// Create a TextTheme and ColorScheme, that we can use to generate ThemeData
+    TextTheme textTheme =
+        (isDark ? ThemeData.dark() : ThemeData.light()).textTheme;
+    ColorScheme colorScheme = ColorScheme(
+      // Map our custom theme to the Material ColorScheme
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      primary: primaryColor,
+      onPrimary: onPrimaryColor,
+      secondary: secondaryColor,
+      onSecondary: onSecondaryColor,
+      error: errorColor,
+      onError: onErrorColor,
+      surface: backgroundColor,
+      onSurface: blackColor,
+    );
+
+    /// Now that we have ColorScheme and TextTheme, we can create the ThemeData
+    /// Also add on some extra properties that ColorScheme seems to miss
+    var theme =
+        ThemeData.from(textTheme: textTheme, colorScheme: colorScheme).copyWith(
+      textSelectionTheme: TextSelectionThemeData(cursorColor: primaryColor),
+      highlightColor: primaryColor,
+    );
+
+    /// Return the themeData which MaterialApp can now use
+    return theme;
+  }
+}
